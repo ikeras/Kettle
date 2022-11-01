05f4084febb46eed407290b14ac458b925fb8768
Anson Horton <ansonh@microsoft.com>
2022-11-01 16:15:52 -0700

    Enable the game ROM rock, paper, scissors to work!
    * Enable additional instructions that RPS needs
        * 0x00ee The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
        * 0x2nnn Put current PC on the top of the stack. The PC is then set to nnn.
        * 0x3xkk Skip next instruction if Vx = kk.
        * 0x4xkk Skip next instruction if Vx != kk.
        * 0x8xy7 Set Vx = Vy - Vx, set VF = NOT borrow.
        * 0xcxkk Set Vx = random byte AND kk.
        * 0xfx0a Wait for a key press, store the value of the key in Vx.
        * 0xfx1e Set I = I + Vx.Set overflow flag as needed.
        * 0xfx29 Set I = location of sprite for digit Vx.
    * Implement press and release key on IChip8Emulator
    * Fix small issue with locking display when clearing it
    * Add built-in font that chip-8 uses

--- Emulation/CPU.cs
+++ Emulation/CPU.cs
@@ -2,16 +2,42 @@
 
 internal class CPU
 {
+    private const byte smallFontHeight = 5;
+    private const ushort smallFontMemoryOffset = 0x00;
+
+    private static readonly byte[] smallFont = new byte[]
+    {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
     private int displayWidth;
     private int displayHeight;
     private byte[] display;
+    private readonly bool[] keys;
     private readonly byte[] memory;
 
     private readonly byte[] registers;
     private ushort i;
+    private int lastKeyPressed;
+    private int numberOfKeysPressed;
     private byte soundTimer;
     private byte delayTimer;
-    private readonly Stack<byte> stack;
+    private readonly Stack<ushort> stack;
     private ushort pc;
 
     public CPU(byte[] memory)
@@ -19,13 +45,18 @@ internal class CPU
         this.memory = memory;
         this.displayHeight = 32;
         this.displayWidth = 64;
+        this.keys = new bool[16];
         this.display = new byte[this.displayHeight * this.displayWidth];
         this.registers = new byte[16];
-        this.stack = new Stack<byte>();
+        this.stack = new();
         this.soundTimer = 0;
         this.delayTimer = 0;
+        this.lastKeyPressed = 0x0;
+        this.numberOfKeysPressed = 0;
         this.i = 0;
         this.pc = 0x200;
+
+        Array.Copy(CPU.smallFont, 0, this.memory, CPU.smallFontMemoryOffset, CPU.smallFont.Length);
     }
 
     public int DisplayWidth => this.displayWidth;
@@ -65,28 +96,116 @@ internal class CPU
         switch (instruction)
         {
             case 0x0:
-                if (nnn == 0x0e0)
+                switch (nnn)
                 {
-                    Array.Clear(this.display);
+                    case 0x0e0:
+                        lock (this.display)
+                        {
+                            Array.Clear(this.display);
+                        }
+                        break;
+                    case 0x0ee:
+                        this.pc = this.stack.Pop();
+                        break;
+                    default:
+                        throw new NotImplementedException();
                 }
                 break;
             case 0x1:
                 this.pc = nnn;
                 break;
-            case 0x06:
+            case 0x2:
+                this.stack.Push(this.pc);
+                this.pc = nnn;
+                break;
+            case 0x3:
+                if (this.registers[x] == kk)
+                {
+                    this.pc += 2;
+                }
+                break;
+            case 0x4:
+                if (this.registers[x] != kk)
+                {
+                    this.pc += 2;
+                }
+                break;
+            case 0x6:
                 this.registers[x] = kk;
                 break;
-            case 0x07:
+            case 0x7:
                 this.registers[x] += kk;
+                break;
+            case 0x8:
+                switch (n)
+                {
+                    case 0x7:
+                        int result = this.registers[y] - this.registers[x];
+                        this.registers[0xf] = (byte)(result >= 0 ? 1 : 0);
+                        this.registers[x] = (byte)result;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
                 break;
             case 0xa:
                 this.i = nnn;
                 break;
+            case 0xc:
+                this.registers[x] = (byte)(Random.Shared.Next() & kk);
+                break;
             case 0xd:
                 this.DrawSprite(x, y, n);
                 break;
+            case 0xf:
+                switch (kk)
+                {
+                    case 0x0a:
+                        if (this.numberOfKeysPressed > 0)
+                        {
+                            this.registers[x] = (byte)this.lastKeyPressed;
+                        }
+                        else
+                        {
+                            this.pc -= 2;
+                        }
+                        break;
+                    case 0x1e:
+                        uint result = (uint)(this.i + this.registers[x]);
+                        if (result > 0xFFF)
+                        {
+                            this.registers[0xf] = 1;
+                        }
+                        this.i = (ushort)result;
+                        break;
+                    case 0x29:
+                        this.i = (ushort)(CPU.smallFontMemoryOffset + (this.registers[x] * CPU.smallFontHeight));
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+                break;
             default:
-                throw new Exception();
+                throw new NotImplementedException();
+        }
+    }
+
+    public void PressKey(int key)
+    {
+        if (!this.keys[key])
+        {
+            this.numberOfKeysPressed++;
+            this.keys[key] = true;
+            this.lastKeyPressed = key;
+        }
+    }
+
+    public void ReleaseKey(int key)
+    {
+        if (this.keys[key])
+        {
+            this.numberOfKeysPressed--;
+            this.keys[key] = false;
         }
     }
 
